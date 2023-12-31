6863f89a8bbc7d395ca7702145816548b4217655
Guimaraes <caique.guimaraesdeoliveira306@gmail.com>
2023-12-31 13:50:31 -0300

    Projeto pronto

---
+++ src/App.java
@@ -0,0 +1,17 @@
+public class App {
+      public static void main(String[] args) {
+            Iphone iphone = new Iphone();
+            
+            iphone.atender();
+            iphone.ligar();
+            iphone.iniciarCorrerioVoz();
+
+            iphone.adicionarNovaAba();
+            iphone.atualizarPagina();
+            iphone.exibirPagina();
+
+            iphone.pausarMusica();
+            iphone.selecionarMúsica();
+            iphone.tocarMusica();
+      }
+}
---
+++ src/Iphone.java
@@ -0,0 +1,37 @@
+public class Iphone implements AparelhoTelefonico, NavegadorInternet, ReprodutorMusical{
+    public void ligar() {
+        System.out.println("Ligando");
+    }
+
+    public void atender() {
+        System.out.println("Atendendo");
+    }
+
+    public void iniciarCorrerioVoz() {
+        System.out.println("Iniciando correio de voz");
+    }
+
+    public void exibirPagina() {
+        System.out.println("Exibindo página");
+    }
+
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba");
+    }
+
+    public void atualizarPagina() {
+        System.out.println("Atualizando página");
+    }
+
+    public void tocarMusica() {
+        System.out.println("Tocando música");
+    }
+
+    public void pausarMusica() {
+        System.out.println("Pausando música");
+    }
+
+    public void selecionarMúsica() {
+        System.out.println("Selecionando música");
+    }
+}
